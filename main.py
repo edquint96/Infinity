3794c75d99b9abeef4582da0463ed094505e9357
ELITEBOOK <jeduardo.quintanarpozos@gmail.com>
2023-01-21 13:14:59 -0600

    funcionó XD

--- main.py
+++ main.py
@@ -2,37 +2,42 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('RX.png')
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def main():
+    img = cv.imread('RX.png')
 
 
-# New dummy image that will contain the adjustments
-img2= np.zeros(img.shape, img.dtype)
+    # New dummy image that will contain the adjustments
+    img2= np.zeros(img.shape, img.dtype)
 
-# Defining alpha and beta:
-alpha = 1  # Contrast Control [1.0-3.0]
-beta = 10   # Brightness Control [0-100]
+    # Defining alpha and beta:
+    alpha = 1  # Contrast Control [1.0-3.0]
+    beta = 5  # Brightness Control [0-100]
 
-# Scaling and converting the image contrast and brightness
-img2= cv.convertScaleAbs(img, alpha=alpha, beta=beta)
+    # Scaling and converting the image contrast and brightness
+    img2= cv.convertScaleAbs(img, alpha=alpha, beta=beta)
 
-# Displaying the adusted image
-cv.imshow('Resulting Image', img2)
-img = cv.imread('RX.png')
-cv.imshow('Original', img)
+    # Displaying the adusted image
+    cv.imshow('Resulting Image', img2)
+    img = cv.imread('RX.png')
+    cv.imshow('Original', img)
 
-blank = np.zeros(img2.shape[:2], dtype='uint8')
+    blank = np.zeros(img2.shape[:2], dtype='uint8')
 
-center_coordinates = (155, 155)
+    center_coordinates = (160, 155)
 
-radius = 110
+    radius = 115
 
-mask = cv.circle(blank, center_coordinates, radius, 255, -1)
+    mask = cv.circle(blank, center_coordinates, radius, 255, -1)
 
-masked = cv.bitwise_and(img, img, mask=mask)
-cv.imshow('Output', masked)
+    masked = cv.bitwise_and(img, img, mask=mask)
+    cv.imshow('Output', masked)
 
 
-imgprocesed= cv.fastNlMeansDenoisingColored(masked,None,10,10,9,21)
-plt.subplot(121),plt.imshow(img)
-plt.subplot(122),plt.imshow(imgprocesed)
-plt.show()
+    imgprocesed= cv.fastNlMeansDenoisingColored(masked,None,10,10,9,21)
+    plt.subplot(121),plt.imshow(img)
+    plt.subplot(122),plt.imshow(imgprocesed)
+    plt.show()
+    return imgprocesed
